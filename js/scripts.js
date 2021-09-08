5988b85995282ce2af5d2ad6d5ac450e1584bd4f
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-08 19:19:46 +0300

    Add a submit form event handler and event.preventDefault() to prevent default

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,12 @@ var add=function(number1,number2){
   };
 
   $(document).ready(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
 
-    var number1=parseInt($("#add1").val());
-    var number2=parseInt($("#add2").val());
-    var result=add(number1,number2);
-    alert(result);
+      var number1=parseInt($("#add1").val());
+      var number2=parseInt($("#add2").val());
+      var result=add(number1,number2);
+      alert(result);
+    });
   });

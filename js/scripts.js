8b6b4891f3fbef6537bd69d1635da5c9eac9e782
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-08 18:44:49 +0300

    Modify the user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,11 @@ var add=function(number1,number2){
   var divide = function(number1, number2) {
     return number1 / number2;
   };
-  var number1=parseInt(prompt("Enter a number:"));
-  var number2=parseInt(prompt("Enter another number:"));
-  var result=add(number1,number2);
-  alert(result);
+
+  $(document).ready(function() {
+
+    var number1=parseInt(prompt("Enter a number:"));
+    var number2=parseInt(prompt("Enter another number:"));
+    var result=add(number1,number2);
+    alert(result);
+  });

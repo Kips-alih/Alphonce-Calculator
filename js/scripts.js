ad407c858c668cfcb64fe6948e88e908decf6233
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-09 09:48:43 +0300

    Add subtract user interface logic, rearrange the code

--- js/scripts.js
+++ js/scripts.js
@@ -18,17 +18,17 @@ var add=function(number1,number2){
       var number1=parseInt($("#add1").val());
       var number2=parseInt($("#add2").val());
       var result=add(number1,number2);
-      $("#output").text(result);
-
+      $("#divisionOutput").text(result);
     });
 
-    $("form#divide").submit(function(event) {
+    $("form#subtract").submit(function(event) {
       event.preventDefault();
 
-      var number1=parseInt($("#divide1").val());
-      var number2=parseInt($("#divide2").val());
-      var result1=divide(number1,number2);
-      $("#output1").text(result1);
+      var number1=parseInt($("#subtract1").val());
+      var number2=parseInt($("#subtract2").val());
+      var result=subtract(number1,number2);
+      $("#subtractOutput").text(result);
+
     });
 
     $("form#multiply").submit(function(event) {
@@ -36,7 +36,17 @@ var add=function(number1,number2){
 
       var number1=parseInt($("#multiple1").val());
       var number2=parseInt($("#multiple2").val());
-      var result1=multiply(number1,number2);
-      $("#output2").text(result1);
+      var result=multiply(number1,number2);
+      $("#multiplicationOutput").text(result);
     });
+
+    $("form#divide").submit(function(event) {
+      event.preventDefault();
+
+      var number1=parseInt($("#divide1").val());
+      var number2=parseInt($("#divide2").val());
+      var result=divide(number1,number2);
+      $("#divisionOutput").text(result);
+    });
+
   });

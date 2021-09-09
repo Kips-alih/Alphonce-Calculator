0851a239dbad08a7087b9fcabc027ceeea2355f3
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-09 09:34:58 +0300

    Add the user interface logic for division and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,22 @@ var add=function(number1,number2){
       $("#output").text(result);
 
     });
+
+    $("form#divide").submit(function(event) {
+      event.preventDefault();
+
+      var number1=parseInt($("#divide1").val());
+      var number2=parseInt($("#divide2").val());
+      var result1=divide(number1,number2);
+      $("#output1").text(result1);
+    });
+
+    $("form#multiply").submit(function(event) {
+      event.preventDefault();
+
+      var number1=parseInt($("#multiple1").val());
+      var number2=parseInt($("#multiple2").val());
+      var result1=multiply(number1,number2);
+      $("#output2").text(result1);
+    });
   });
